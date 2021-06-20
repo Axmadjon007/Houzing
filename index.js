0c1093fad03f17f467d8ad10fb81ebd32c9fb3b9
axmadj <axmadjon>
2021-06-20 12:32:02 +0500

    project

---
+++ index.js
@@ -0,0 +1,54 @@
+
+            // 1-Masala
+
+    var a = 2
+
+    console.log("1-Masala")
+    console.log(P=4*a)
+    console.log("====")
+
+            // 2-Masala
+
+    var a = 3
+
+    console.log("2-Masala")
+    console.log(s=a*a)
+    console.log("====")
+
+
+
+        // 3-Masala
+    var a = 2
+    var b = 3
+    var s = a * b
+    a = 4
+    b = 6
+    p = 2 *(a+b)
+
+    console.log("3-Masala")
+    console.log(s)
+    console.log(p)
+    console.log("====")
+
+
+
+        // 4-Masala
+    var d = 3
+    const pi=3.14
+
+    console.log("4-Masala")
+    console.log(L=pi*d)
+    console.log("====")
+
+
+      // 5-Masala
+var a =2
+var v = a**3
+var s= 6*a**2
+
+console.log("5-Masala")
+
+
+console.log(v)
+console.log(s)
+console.log("====")
