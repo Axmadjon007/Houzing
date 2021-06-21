6746605fff7c010af35538f3487daccd86dbdff8
axmadj <axmadjon>
2021-06-21 13:00:55 +0500

    js 1 vazifa tayyor

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 
-            // 1-Masala
-
+        // 1-Masala
+        console.log("1-masala")
     var a = 2
 
-    console.log("1-Masala")
     console.log(P=4*a)
-    console.log("====")
+    console.log("======")
+
 
             // 2-Masala
-
+            console.log("2-masala")
     var a = 3
 
-    console.log("2-Masala")
     console.log(s=a*a)
-    console.log("====")
+    console.log("=======")
 
 
 
         // 3-Masala
+        console.log("3-masala")
     var a = 2
     var b = 3
     var s = a * b
@@ -25,30 +25,102 @@
     b = 6
     p = 2 *(a+b)
 
-    console.log("3-Masala")
     console.log(s)
     console.log(p)
-    console.log("====")
+    console.log("========")
+
 
 
 
         // 4-Masala
+        console.log("4-masala")
     var d = 3
     const pi=3.14
 
-    console.log("4-Masala")
     console.log(L=pi*d)
-    console.log("====")
+    console.log("========")
 
 
-      // 5-Masala
-var a =2
-var v = a**3
-var s= 6*a**2
 
-console.log("5-Masala")
+        // 5-Masala
+        console.log("5-masala")
+    var a =2
+    var v = a**3
+    var s= 6*a**2
+
+    console.log(v)
+    console.log(s)
+    console.log("========")
 
 
-console.log(v)
-console.log(s)
-console.log("====")
+        
+      // 6-Masala
+      console.log("6-masala")
+    var a = 4
+    var b = 6
+    var c = 2
+    var v = a+b+
+
+        s + v*2
+    console.log(v)
+    console.log(s)
+    console.log("==========")
+
+
+      // 7-Masala
+      console.log("7-masala")
+    var R = 4
+
+    console.log(L=2*pi*R)
+    console.log(S=pi*R*R)
+    console.log("==========")
+
+
+
+      // 8-Masala
+      console.log("8-masala")      
+    var a = 4
+    var b = 30
+
+    console.log((a*b)/2)
+
+
+        // 9-Masala
+      console.log("9-Masala")
+    var a = 4
+    var b = 5
+
+    console.log((a*b)**(1/2))
+    console.log("==========")
+
+
+
+        // 10-Masala
+
+        console.log("10-Masala")
+    var a = 6
+    var b = 4
+
+    console.log(a+b)
+    console.log(a*b)
+    console.log(a*a)
+    console.log(b*b)
+    console.log("==========")
+
+
+
+
+
+
+
+        // 11-Masala
+        console.log("11-masala")
+    var a = 8
+    var b = 4
+
+    console.log(a+b)
+    console.log(a*b)
+    console.log(a>0)
+    console.log(b<0) 
+    console.log("==========")
+
